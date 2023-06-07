7b2c78eb6f1408076ae7c43159126cc9397b1d89
JaysonSimon1 <75974988+JaysonSimon1@users.noreply.github.com>
2023-06-07 11:07:10 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:djo_homepage/views/home/home_view.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyAXQ-ynRadjzk4haN_i_PLfcjz-bgI-P44",
+      appId: "1:294416703656:web:6f89a468b98a6e20d9229d",
+      messagingSenderId: "294416703656",
+      projectId: "djothebarberdatabase",
+    ),
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,3 +25,26 @@ class MyApp extends StatelessWidget {
         );
   }
 }
+
+// functie voor pop-up te laten verschijnen
+  showPopUpForm(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Center(
+          child: Container(
+            clipBehavior: Clip.antiAlias,
+            height: popUpForm.popUpFormHeight,
+            width: popUpForm.popUpFormWidth,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              color: Colors.white,
+            ),
+            child: PopUpForm(),
+          ),
+        );
+      },
+    );
+  }
+}
+
